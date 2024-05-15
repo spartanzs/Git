5e8ff86c9d2e101dcb7c62d48c05cf90321cf33b
munkam <munesukambani@gmail.com>
2024-05-15 21:34:33 +0200

    sec commit

--- test.js
+++ test.js
@@ -1,13 +1,49 @@
-<html>  
+<!DOCTYPE html>
 
-<body>  
+<html>
 
-<script type="text/javascript">  
+<head>
 
- alert("Hello World");  
+</head>
 
-</script>  
+<body style = "text-align: center; font-size: 20px;">
 
-</body>  
+<h1> Welcome to the javaScript world!! </h1>
 
-</html>  
+Enter a particular number: <input id = "num">
+
+<br><br>
+
+<button onclick = "fact()"> Please type any Factorial number </button>
+
+<p id = "res"></p>
+
+<script>
+
+function fact(){
+
+var i, num, f;
+
+f = 1;
+
+num = document.getElementById("num").value;
+
+for(i = 1; i <= num; i++)  
+
+{
+
+f = f * i;
+
+}
+
+i = i - 1;  
+
+document.getElementById("res").innerHTML = "The factorial of the number " + i + " is: " + f ;
+
+}
+
+</script>
+
+</body>
+
+</html>
